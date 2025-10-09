f9f064cd96d359152380f6948a6d3183c2dfedf4
SamyuktaNair <nsamyukta11@gmail.com>
2025-10-09 15:42:39 +0530

    Change

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <div>Change</div>
+        <div>Change </div>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
